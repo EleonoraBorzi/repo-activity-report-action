b736d24ed73bac4f92ed21d68e05ddad6f54309c
Henke <kultala@kth.se>
2021-04-24 20:05:09 +0200

    Calculate average PR and issue open time

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 from github import Github
 import sys 
 import os
+import datetime
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 
@@ -26,11 +27,59 @@ def list_reviwed_issues(url):
 def list_reviwed_pr(url):
     print()
 
+# https://docs.github.com/en/rest/reference/pulls
+# https://stackoverflow.com/questions/17423598/how-can-i-get-a-list-of-all-pull-requests-for-a-repo-through-the-github-api 
+# https://stackoverflow.com/questions/18795713/parse-and-format-the-date-from-the-github-api-in-python/18795714 
+# Merged implies closed, but it can be closed without being merged. Date is 'None' if it isn't merged/closed.
 def average_pr_close_time(url):
-    print()
+    url = url + "/pulls"
+    params = {"state":"all"}
+    payload = requests.get(url=url, headers=headers, params=params).json()
+    closed_durations = []
+    merged_durations = []
+    open_durations = []
+    for it in payload:
+        created_date = datetime.datetime.strptime(it["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+        if it["merged_at"] is not None:
+            merged_date = datetime.datetime.strptime(it["merged_at"], "%Y-%m-%dT%H:%M:%SZ")
+            merged_durations.append((merged_date - created_date).total_seconds())
+        elif it["closed_at"] is not None:
+            closed_date = datetime.datetime.strptime(it["closed_at"], "%Y-%m-%dT%H:%M:%SZ")
+            closed_durations.append((closed_date - created_date).total_seconds())
+        else:
+            today_formatted = datetime.datetime.strptime(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"), "%Y-%m-%dT%H:%M:%SZ")
+            open_durations.append((today_formatted - created_date).total_seconds())
 
+    average_non_merge_close_time = float(sum(closed_durations)) / (len(closed_durations))
+    average_merge_time = float(sum(merged_durations)) / (len(merged_durations))
+    average_still_open_time = float(sum(open_durations)) / (len(open_durations))
+    print("Average time until PR closed but not merged: ", average_non_merge_close_time)
+    print("Average time until PR merged: ", average_merge_time)
+    print("Average time opened for still open PRs: ", average_still_open_time)
+
+# Can probably be combined with the PR one, especially since this also has access to the pull requests
 def average_issue_close_time(url):
-    print()
+    url = url + "/issues"
+    params = {"state":"all"}
+    payload = requests.get(url=url, headers=headers, params=params).json()
+    closed_durations = []
+    open_durations = []
+    for it in payload:
+        if "pull_request" in it:
+            # 'issues' return both issues and pull requests, so we have to get rid of the pull requests
+            continue
+        created_date = datetime.datetime.strptime(it["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+        if it["closed_at"] is not None:
+            closed_date = datetime.datetime.strptime(it["closed_at"], "%Y-%m-%dT%H:%M:%SZ")
+            closed_durations.append((closed_date - created_date).total_seconds())
+        else:
+            today_formatted = datetime.datetime.strptime(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"), "%Y-%m-%dT%H:%M:%SZ")
+            open_durations.append((today_formatted - created_date).total_seconds())
+
+    average_non_merge_close_time = float(sum(closed_durations)) / (len(closed_durations))
+    average_still_open_time = float(sum(open_durations)) / (len(open_durations))
+    print("Average time until issue closed: ", average_non_merge_close_time)
+    print("Average time opened for still open issues: ", average_still_open_time)
 
 def average_pr_response_time(url):
     print()
@@ -51,7 +100,7 @@ def main():
     #url = "https://api.github.com/repo/" + str(repo_name)
 
     #dummy input
-    repo_name = "jhy/jsoup"
+    repo_name = "EleonoraBorzis/group-validity-action" #"jhy/jsoup"
     url = "https://api.github.com/repos/" + str(repo_name)
 
     reviewed_pr(url)
