4c6b184ddfc9ef0ecc7aa51eb3495a9a08624d16
EleonoraBorzis <47255376+EleonoraBorzis@users.noreply.github.com>
2021-04-28 09:59:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def write_comment(git_token, repo_name, issue_number, report):
     g = Github(git_token)
     repo = g.get_repo(repo_name)
     pr = repo.get_pull(int(issue_number))
-    pr.create_issue_comment("### " + report)
+    pr.create_issue_comment("<p>" + report + "</p>")
 
 def unreviewed_pr(pr_list):
     pr_nr = []
