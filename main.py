3b8d3313d62562601b3b4ddcf3e6441ecffbd324
EleonoraBorzis <47255376+EleonoraBorzis@users.noreply.github.com>
2021-04-30 10:27:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -400,7 +400,7 @@ def lizard(head_path, include_warnings=False):
     #os.system('cmd /k "cd head"')
    # os.system('cmd /k "ls"')
     
-    stream = os.popen("lizard head")
+    stream = os.popen("lizard")
     output = stream.read()
     search_string = "Total nloc"
     if include_warnings:
